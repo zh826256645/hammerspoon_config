1fe671e1db4040b074844b06194f2d379d0b056e
钟浩 <zh826256645@gmail.com>
2020-07-06 11:05:54 +0800

    perf(帮助): 更新帮助文档

--- modules/helps.lua
+++ modules/helps.lua
@@ -1,26 +1,32 @@
 -- 快捷键说明文档
 local helpMenubar = hs.menubar.new()
 local menuData = {}
+local subWindowMenuData = {}
+local subProgramMenuData = {}
 
 function windowHelps()
-    table.insert(menuData, { title="window 快捷键" })
+    table.insert(menuData, { title="窗口快捷键", menu=subWindowMenuData})
+    table.insert(subWindowMenuData, { title="左吸附              ^⌥⌘←"})
+    table.insert(subWindowMenuData, { title="右吸附              ^⌥⌘→"})
+    table.insert(subWindowMenuData, { title="上吸附              ^⌥⌘↑"})
+    table.insert(subWindowMenuData, { title="下吸附              ^⌥⌘↓"})
+    table.insert(subWindowMenuData, { title="-" })
+    table.insert(subWindowMenuData, { title="左上角              ^⌥⇧←"})
+    table.insert(subWindowMenuData, { title="右下角              ^⌥⇧→"})
+    table.insert(subWindowMenuData, { title="右上角              ^⌥⇧↑"})
+    table.insert(subWindowMenuData, { title="左下角              ^⌥⇧↓"})
+    table.insert(subWindowMenuData, { title="-" })
+    table.insert(subWindowMenuData, { title="到上个屏幕       ⌥⇧←"})
+    table.insert(subWindowMenuData, { title="到下个屏幕       ⌥⇧→"})
+    table.insert(subWindowMenuData, { title="-" })
+    table.insert(subWindowMenuData, { title="最大化              ^⌥⌘M"})
+    table.insert(subWindowMenuData, { title="全屏幕              ^⌥⌘F"})
+    table.insert(subWindowMenuData, { title="屏幕居中          ^⌥⌘C"})
+
     table.insert(menuData, { title="-" })
-    table.insert(menuData, { title="左吸附              ^⌥⌘←"})
-    table.insert(menuData, { title="右吸附              ^⌥⌘→"})
-    table.insert(menuData, { title="上吸附              ^⌥⌘↑"})
-    table.insert(menuData, { title="下吸附              ^⌥⌘↓"})
-    table.insert(menuData, { title="-" })
-    table.insert(menuData, { title="左上角              ^⌥⇧←"})
-    table.insert(menuData, { title="右下角              ^⌥⇧→"})
-    table.insert(menuData, { title="右上角              ^⌥⇧↑"})
-    table.insert(menuData, { title="左下角              ^⌥⇧↓"})
-    table.insert(menuData, { title="-" })
-    table.insert(menuData, { title="到上个屏幕       ⌥⇧←"})
-    table.insert(menuData, { title="到下个屏幕       ⌥⇧→"})
-    table.insert(menuData, { title="-" })
-    table.insert(menuData, { title="最大化              ^⌥⌘M"})
-    table.insert(menuData, { title="全屏幕              ^⌥⌘F"})
-    table.insert(menuData, { title="屏幕居中          ^⌥⌘C"})
+    table.insert(menuData, { title="程序快捷键", menu=subProgramMenuData})
+    table.insert(subProgramMenuData, { title="iTerm              ⌃⌥T"})
+    table.insert(subProgramMenuData, { title="Finder              ⌘E"})
     
 end
 
